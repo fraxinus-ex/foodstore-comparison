1c728ee1b5d0e70936582eb00c8e0175856a9d76
Kadri Saare <kadri.saare@gmail.com>
2021-12-06 09:30:38 +0200

    Initial commit

---
+++ src/main/java/com/example/foodstorecomparison/FoodstoreComparisonApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodstorecomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodstoreComparisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodstoreComparisonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodstorecomparison/FoodstoreComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodstorecomparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodstoreComparisonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
